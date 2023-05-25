001e35bc0df0330b5d10d3ab7649745e0628251d
Levon Ghukasyan <lyovghukasyan@gmail.com>
2023-05-25 05:56:12 +0400

    setup prisma

--- server/apps/backend/src/main.ts
+++ server/apps/backend/src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 import { Logger } from '@wr/logger';
+// import data from '@wr/data-source';
 
 async function bootstrap() {
   const mainLogger = new Logger('APP');
   const app = await NestFactory.create(AppModule, {
     logger: mainLogger,
   });
+
+  // await data.$connect();
+
+  console.log('connected');
 
   const appPort = 3000;
 
