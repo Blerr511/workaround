28961bae3ef80e9f84086dadcbd215a02d6cfc20
Levon Ghukasyan <lyovghukasyan@gmail.com>
2023-05-31 00:34:47 +0400

    minor fixes on web client

--- client/apps/web-client/src/pages/_app.tsx
+++ client/apps/web-client/src/pages/_app.tsx
@@ -1,19 +1,23 @@
-import '@/styles/globals.css';
+import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { HelloWorld } from '@wr/ui';
 import { MYs } from '@wr/backend-api';
+import { ob } from '@/pages/api/test';
+
+console.log(ob);
 
 const my: MYs = {
   email: 'asd',
   id: 123,
-  lastName: 1223,
+  lastName: '1223',
+  name: '123',
 };
-
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <HelloWorld />
+      {JSON.stringify(ob)}
       <Component {...pageProps} />
     </>
   );
